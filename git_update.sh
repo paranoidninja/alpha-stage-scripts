9a129063618a596f7ab605c3cc9236a5726cc59f
Paranoid Ninja <paranoidninja@protonmail.com>
2017-05-09 09:50:11 +0530

    Update git_update.sh

--- git_update.sh
+++ git_update.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+##Author : Paranoid Ninja
+##Email  : paranoidninja@protonmail.com
+##Descr  : Update all git repos in current directory
+
 for d in ./*/; do (cd "$d" && echo "Updating $d" && git pull); done
