39d73730b615858f6eb4e7427c2b989c5435e6d3
Paranoid Ninja <paranoidninja@protonmail.com>
2017-05-09 09:08:55 +0530

    Update dga.sh

--- dga.sh
+++ dga.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 ##Author : Paranoid Ninja
-##Email : paranoidninja@protonmail.com
-##Simple Domain Generation Algorithm Script for testing DNS Bro Scripts
+##Email  : paranoidninja@protonmail.com
+##Descr  : Simple Domain Generation Algorithm Script for testing DNS Bro Scripts
 
 temp_name=`pwgen 13 1`
 nslookup $temp_name.com | grep -v 127 | grep -i -e 'Name\|Address\|NXDOMAIN'
