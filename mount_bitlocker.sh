ffe2c8a30aa0309f5a84aa8b958f6e8de07f280a
Paranoid Ninja <paranoidninja@protonmail.com>
2019-04-03 12:14:58 +0530

    Update mount_bitlocker.sh

--- mount_bitlocker.sh
+++ mount_bitlocker.sh
@@ -4,15 +4,24 @@
 ##Email  : paranoidninja@protonmail.com
 ##Descr  : Mount Bitlocker at mounted location
 
+#!/bin/bash
+
 mountee="/media/armory_locker"
 mounted="/media/armory_mounted"
-
 if [[ $EUID == 0 ]]; then
 	bitpart=`fdisk -l | egrep "HPFS|NTFS" | cut -f 1 -d" "`
 	echo $bitpart
 	mkdir $mountee
 	mkdir $mounted
-	sudo dislocker -r -V $bitpart -u -- $mountee
-	mount -r -o loop /media/armory_locker/dislocker-file /media/armory_mounted
-	echo -e "Mounted at $mounted\n"
+	#if the user has a password, use 0 as first argument
+    if [[ $1 == 0 ]]; then
+        dislocker -r -V $bitpart -u -- $mountee
+        mount -r -o loop /media/armory_locker/dislocker-file /media/armory_mounted
+        echo -e "Mounted at $mounted\n"
+    else 
+        dislocker -r -V $bitpart -p -- $mountee
+        mount -r -o loop /media/armory_locker/dislocker-file /media/armory_mounted
+        echo -e "Mounted at $mounted\n"
+    fi
+    #if the user has a recovery key, use 1 as first argument
 fi
